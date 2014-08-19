6c465476419112cd492cc06464276da800fc73e2
Donghao Ren <donghao.ren@gmail.com>
2014-08-19 11:29:52 -0700

    Added OpenGL binding for gl* functions.

---
+++ src/glubind.h
@@ -0,0 +1,17 @@
+/*
+ * glubind.h
+ *
+ *  Created on: Jun 15, 2009
+ *      Author: nicolas
+ */
+
+#ifndef GLUBIND_H_
+#define GLUBIND_H_
+
+
+#include <v8.h>
+
+
+v8::Handle<v8::ObjectTemplate> createGlu(void);
+
+#endif /* GLUBIND_H_ */
