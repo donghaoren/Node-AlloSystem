9c33d74391e4196efbb51e4e7d7ac6875f029c9d
Donghao Ren <donghao.ren@gmail.com>
2014-08-29 15:18:53 -0700

    Updates to findfreeimage.

--- cmake_modules/FindFreeImage.cmake
+++ cmake_modules/FindFreeImage.cmake
@@ -6,46 +6,42 @@
 # FREEIMAGE_INCLUDE_PATH
 # FREEIMAGE_LIBRARY
 #
+INCLUDE ( LibFindMacros )
+LIBFIND_PKG_CHECK_MODULES ( ALLOCORE_PKGCONF liballocore )
 
-IF (WIN32)
-	FIND_PATH( FREEIMAGE_INCLUDE_PATH FreeImage.h
-		${PROJECT_SOURCE_DIR}/extern/FreeImage
-		DOC "The directory where FreeImage.h resides")
-	FIND_LIBRARY( FREEIMAGE_LIBRARY
-		NAMES FreeImage freeimage
-		PATHS
-		${PROJECT_SOURCE_DIR}/FreeImage
-		DOC "The FreeImage library")
-ELSE (WIN32)
-	FIND_PATH( FREEIMAGE_INCLUDE_PATH FreeImage.h
-		/usr/include
-		/usr/local/include
-		/sw/include
-		/opt/local/include
-		DOC "The directory where FreeImage.h resides")
-	FIND_LIBRARY( FREEIMAGE_LIBRARY
-		NAMES FreeImage freeimage
-		PATHS
-		/usr/lib64
-		/usr/lib
-		/usr/local/lib64
-		/usr/local/lib
-		/sw/lib
-		/opt/local/lib
-		DOC "The FreeImage library")
-ENDIF (WIN32)
+FIND_PATH( FREEIMAGE_INCLUDE_PATH
+	NAMES FreeImage.h
+	PATHS
+	${ALLOCORE_PKGCONF_INCLUDE_DIRS}
+	/usr/include
+	/usr/local/include
+	/sw/include
+	/opt/local/include
+	DOC "The directory where FreeImage.h resides")
+FIND_LIBRARY( FREEIMAGE_LIBRARY
+	NAMES FreeImage freeimage
+	PATHS
+	${ALLOCORE_PKGCONF_LIBRARY_DIRS}
+	/usr/lib64
+	/usr/lib
+	/usr/local/lib64
+	/usr/local/lib
+	/sw/lib
+	/opt/local/lib
+	DOC "The FreeImage library")
 
 SET(FREEIMAGE_LIBRARIES ${FREEIMAGE_LIBRARY})
 
-IF (FREEIMAGE_INCLUDE_PATH AND FREEIMAGE_LIBRARY)
-	SET( FREEIMAGE_FOUND TRUE CACHE BOOL "Set to TRUE if FreeImage is found, FALSE otherwise")
-ELSE (FREEIMAGE_INCLUDE_PATH AND FREEIMAGE_LIBRARY)
-	SET( FREEIMAGE_FOUND FALSE CACHE BOOL "Set to TRUE if FreeImage is found, FALSE otherwise")
-ENDIF (FREEIMAGE_INCLUDE_PATH AND FREEIMAGE_LIBRARY)
+IF ( FREEIMAGE_INCLUDE_PATH AND FREEIMAGE_LIBRARY )
+    SET( FREEIMAGE_FOUND TRUE CACHE BOOL "Set to TRUE if FreeImage is found, FALSE otherwise")
+ELSE ( FREEIMAGE_INCLUDE_PATH AND FREEIMAGE_LIBRARY )
+	SET ( FREEIMAGE_FOUND FALSE CACHE BOOL "Set to TRUE if FreeImage is found, FALSE otherwise" )
+ENDIF ( FREEIMAGE_INCLUDE_PATH AND FREEIMAGE_LIBRARY )
 
-MARK_AS_ADVANCED(
+MARK_AS_ADVANCED (
 	FREEIMAGE_FOUND
 	FREEIMAGE_LIBRARY
 	FREEIMAGE_LIBRARIES
-	FREEIMAGE_INCLUDE_PATH)
+	FREEIMAGE_INCLUDE_PATH
+)
 
